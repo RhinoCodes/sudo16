ae86d0ba8eeb83f7e740d2b4c6ec6882615f24e2
sudo killall windows <51250021+RhinoCodes@users.noreply.github.com>
2023-11-21 17:07:03 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source "$HOME/.cargo/env"
+export PATH="$PATH:$HOME/.cargo/bin"
 cargo install
 cargo install wasm-pack
 wasm-pack build --target no-modules
